11037589e4ba6e43be9dac7d2f3c25eac462bf02
Vladimir <madfrog@e1.ru>
2022-09-13 20:08:27 +0300

    Created Task 4

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿try
 {
-    Console.WriteLine("Введите первое число ");
+    Console.WriteLine("Введите первое целое число ");
     int x=Convert.ToInt32 (Console.ReadLine());
-    Console.WriteLine("Введите второе число ");
+    Console.WriteLine("Введите второе целое число ");
     int y=Convert.ToInt32 (Console.ReadLine());
-    Console.WriteLine("Введите третье число ");
+    Console.WriteLine("Введите третье целое число ");
     int z=Convert.ToInt32 (Console.ReadLine());
 
     int max=x;
@@ -16,5 +16,5 @@
 }
 catch
 {
-Console.WriteLine("Необходимо вводить числа цифрами");
+Console.WriteLine("Необходимо вводить целые числа цифрами");
 }
