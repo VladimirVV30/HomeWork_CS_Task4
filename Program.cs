6092f8760259270620d5b89b3482eb6b1eb7752c
Vladimir <madfrog@e1.ru>
2022-09-13 19:57:45 +0300

    Created Task 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿try
+{
+    Console.WriteLine("Введите первое число ");
+    int x=Convert.ToInt32 (Console.ReadLine());
+    Console.WriteLine("Введите второе число ");
+    int y=Convert.ToInt32 (Console.ReadLine());
+    Console.WriteLine("Введите третье число ");
+    int z=Convert.ToInt32 (Console.ReadLine());
+
+    int max=x;
+
+    if (y>max) max = y;
+    if (z>max) max = z;
+    
+    Console.WriteLine("Максимальное число "+max);
+}
+catch
+{
+Console.WriteLine("Необходимо вводить числа цифрами");
+}
